f962a03e429d32168911b560f23b4b1d28dda3fc
Harvey Lallave <harvey_lallave@gmail.com>
2017-12-09 03:10:08 +0800

    Connected css + js

--- script/Home.js
+++ script/Home.js
@@ -1,10 +1,4 @@
 
-// https://stackoverflow.com/questions/46155/how-to-validate-email-address-in-javascript
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return /*re.test(email)*/ true;
-}
-
 /*//This function handles the front-end effects/notice when user does not place the correct input.
 function enterValidInput() {
 	//Materialize.toast('Please enter a valid email / pasword', 7000);
@@ -63,7 +57,6 @@ function submitTheForm(username, password) {
         	console.log("error searchResult.js");
         }
      });*/
-	alert("S");
 	document.location.href = 'Dashboard.html';
 }
 
@@ -76,7 +69,7 @@ $("document").ready(function() {
         var password = document.getElementById('password').value;
         
         //Check if email exists, entered and password is entered.
-        if(/*validateEmail(username) && password !== null && password !== ""*/true){
+        if(/*password !== null && password !== ""*/true){
         	// Check if email
         	submitTheForm(username, password);
         	 
@@ -94,7 +87,7 @@ $("document").ready(function() {
         
         // When enter key is pressed
         if((activeElement.id == 'username' || activeElement.id == 'password') && (e.keyCode == 13))
-	        if(/*validateEmail(username) && password !== null && password !== ""*/true)
+	        if(/*password !== null && password !== ""*/true)
 	        	 submitTheForm(username, password);
 	        else enterValidInput();
 
